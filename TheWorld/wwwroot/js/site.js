a6175090fd669c06ef8b1ecd4f9ccb0c592455a4
kmason <kevin.mason@outlook.com>
2017-10-11 15:19:27 -0500

    ch 4 JavaScript Events

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 
 var ele = document.getElementById("username");
 ele.innerHTML = "Bill Johnson"
+
+var main = document.getElementById("main");
+main.onmouseenter = function () {
+    main.style.backgroundColor = "#888";
+};
+
+main.onmouseleave = function () {
+    main.style.backgroundColor = "";
+};
