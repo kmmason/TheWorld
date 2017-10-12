cbb437c00b90a206a39ea203c5101c37b2ed64e1
kmason <kevin.mason@outlook.com>
2017-10-12 14:41:48 -0500

    ch 4 Practical jQuery

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -3,20 +3,32 @@
 (function () {
 
     
-    var ele = $("#username");
-        ele.text("Bill Johnson");
+ //   var ele = $("#username");
+ //       ele.text("Bill Johnson");
+ //
+ //   var main = $("main");
+ //   main.on("mouseenter", function () {
+ //       main.style = "backgroundColor = #888;";
+ //   });
+ //   main.on("mouseleave", function () {
+ //       main.style = "";
+ //   });
+//
+//   var menuItems = $("ul.menu li a");
+//  menuItems.on("click", function () {
+//       var me = $(this);
+//        alert(me.text());
+//    });
 
-    var main = $("main");
-    main.on("mouseenter", function () {
-        main.style = "backgroundColor = #888;";
-    });
-    main.on("mouseleave", function () {
-        main.style = "";
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
     });
 
-    var menuItems = $("ul.menu li a");
-    menuItems.on("click", function () {
-        var me = $(this);
-        alert(me.text());
-    });
 })();
